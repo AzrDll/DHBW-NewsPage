4d88e01dabc96e98da28d3c3b83bcc09122c24e0
AzrDll <noah.vesenjak@gmail.com>
2024-06-17 13:14:47 +0200

    chore: Improve login page validation and error handling

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function checkloginfield(){
     var username = document.getElementById("usr").value;
     var password = document.getElementById("pwd").value;
     if(username == "" || password == ""){
-        alert("exit");
+        alert("Username or Password cannot be empty");
         return false;
     }
 
